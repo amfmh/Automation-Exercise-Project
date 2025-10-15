94656d2101a72d20b9d33d2fcec8e82df42d7662
amfmh <ahmedmotaz932@gmail.com>
2025-10-15 23:40:19 +0300

    commit NO.10 by adding logs in TC08

--- src/test/java/Tests/TC08_VerifyAllProductsAndProductDetailPage.java
+++ src/test/java/Tests/TC08_VerifyAllProductsAndProductDetailPage.java
@@ -39,15 +39,24 @@ public class TC08_VerifyAllProductsAndProductDetailPage {
     @Test
     public void VerifyAllProductsAndProductDetailPage() throws IOException {
         Assert.assertTrue(VerifyUrl(getDriver(), DataUtils.getPropertyValue("environment","HOME_URL")));
-
+        LogsUtils.info("Verify that home page is visible successfully");
 
         new P01_HomePage(getDriver()).clickOnProductsButton();
+        LogsUtils.info("Click on 'Products' button");
+
         Assert.assertTrue(new P06_ProductsPage(getDriver()).veryProductsPageVisibleByAllProductsLabel());
+        LogsUtils.info("Verify user is navigated to ALL PRODUCTS page successfully");
 
 
         Assert.assertTrue(new P06_ProductsPage(getDriver()).verifyProductsListIsVisible());
+        LogsUtils.info("The products list is visible");
 
         new P06_ProductsPage(getDriver()).clickOnViewProductButtonOfFirstProduct();
+        LogsUtils.info("Click on 'View Product' of first product");
+
+        Assert.assertTrue(VerifyUrl(getDriver(), DataUtils.getPropertyValue("environment","FIRST_PRODUCT_DETAILS_URL")));
+        LogsUtils.info("User is landed to product detail page");
+
 
         Assert.assertTrue(new P07_ProductDetailsPage(getDriver()).verifyProductNameIsVisible());
         Assert.assertTrue(new P07_ProductDetailsPage(getDriver()).verifyProductCategoryIsVisible());
@@ -55,6 +64,7 @@ public class TC08_VerifyAllProductsAndProductDetailPage {
         Assert.assertTrue(new P07_ProductDetailsPage(getDriver()).verifyProductAvailabilityIsVisible());
         Assert.assertTrue(new P07_ProductDetailsPage(getDriver()).verifyProductConditionIsVisible());
         Assert.assertTrue(new P07_ProductDetailsPage(getDriver()).verifyProductBrandIsVisible());
+        LogsUtils.info("Verify that detail detail is visible: product name, category, price, availability, condition, brand");
 
     }
 
